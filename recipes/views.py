16d63b752294124710a3e0b4f35d74a655a0d0a6
Eden Alemayehu Gebeta <edengebeta210@gmail.com>
2025-12-15 09:43:11 +0300

    add templates and html files

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render, HttpResponse
-
+recipes = [
+    {
+        'author': 'Eden',
+        'title': 'Doro wet',
+        'directions': 'combine all',
+        'date_posted': 'May 19, 2021',
+    },
+    {
+        'author': 'Naol',
+        'title': 'Dinch wet',
+        'directions': 'combine all',
+        'date_posted': 'May 28, 2021',
+    },
+    {
+        'author': 'Dawit',
+        'title': 'Misr wet',
+        'directions': 'combine all',
+        'date_posted': 'May 09, 2021',
+    }
+]
 # Create your views here.
 def home(request):
-    return render(request, "recipes/home.html")
+    context = {
+        'recipes': recipes,
+        'title': 'Home',
+    }
+    return render(request, "recipes/home.html", context)
     # return HttpResponse("<h1>Welcome to my Recipes App </h1>")
 def about(request):
-    return render(request, "recipes/about.html")
+    return render(request, "recipes/about.html", {'title': 'About'})
     # return HttpResponse("<h1>This is Recipes App to keep track of your recipes.</h1>")
