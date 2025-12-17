19b9197a22ddecc698791d167d426e0a99be40bc
Eden Alemayehu Gebeta <edengebeta210@gmail.com>
2025-12-17 20:12:02 +0300

    Add crispy forms for proper form usage

--- recipeGenerator/settings.py
+++ recipeGenerator/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     #local apps
     'recipes',
     'users',
+    
+    #3rd party
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = ["bootstrap4"]
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
